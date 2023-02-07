905c4ed996c59fbfbd9d745dde03d700c83b68a7
renanmuniz <renan.merlo@paygo.com.br>
2023-02-06 22:16:36 -0300

    Projeto criado

---
+++ kotlinspringrestcrud/src/main/kotlin/com/kotlinspringrestcrud/KotlinspringrestcrudApplication.kt
@@ -0,0 +1,11 @@
+package com.kotlinspringrestcrud
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinspringrestcrudApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinspringrestcrudApplication>(*args)
+}
